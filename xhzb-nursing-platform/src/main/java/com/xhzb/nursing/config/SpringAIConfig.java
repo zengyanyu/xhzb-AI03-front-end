65e3c8c807aa670ad1906787650b25441ecae21c
songyu <songyu@itcast.cn>
2026-07-18 16:24:59 +0800

    day03-SpringAI：集成Ollama

--- xhzb-nursing-platform/src/main/java/com/xhzb/nursing/config/SpringAIConfig.java
+++ xhzb-nursing-platform/src/main/java/com/xhzb/nursing/config/SpringAIConfig.java
@@ -1,7 +1,7 @@
 package com.xhzb.nursing.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,12 +18,21 @@ public class SpringAIConfig {
      * @param openAiChatModel springAI会读取你配置文件中大模型信息封装的大模型数据对象
      * @return
      */
+    // @Bean
+    // public ChatClient chatClient(OpenAiChatModel openAiChatModel){
+    //
+    //     //创建调用大模型的客户端对象
+    //     return ChatClient.builder(openAiChatModel)
+    //             .defaultSystem("您是一家名为“星海智伴”的职业养老机构的客户聊天助手，你的名字叫小智。请以友好、乐于助人和愉快的方式解答用户的各种问题。")
+    //             .build();
+    // }
+
     @Bean
-    public ChatClient chatClient(OpenAiChatModel openAiChatModel){
+    public ChatClient chatClient(OllamaChatModel ollamaChatModel){
 
         //创建调用大模型的客户端对象
-        return ChatClient.builder(openAiChatModel)
-                .defaultSystem("您是一家名为“星海智伴”的职业养老机构的客户聊天助手，你的名字叫小智。请以友好、乐于助人和愉快的方式解答用户的各种问题。")
+        return ChatClient.builder(ollamaChatModel)
+                //.defaultSystem("您是一家名为“星海智伴”的职业养老机构的客户聊天助手，你的名字叫小智。请以友好、乐于助人和愉快的方式解答用户的各种问题。")
                 .build();
     }
 }
